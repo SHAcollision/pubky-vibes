ac4eba66cd09035b7f900608db633c0177155ec0
SHAcollision <127778313+SHAcollision@users.noreply.github.com>
2025-10-13 02:23:25 +0900

    Add PKDNS homeserver tooling

--- pubky-swiss-knife/src/tabs/mod.rs
+++ pubky-swiss-knife/src/tabs/mod.rs
@@ -1,6 +1,7 @@
 pub mod auth;
 pub mod http;
 pub mod keys;
+pub mod pkdns;
 pub mod sessions;
 pub mod state;
 pub mod storage;
@@ -9,9 +10,11 @@ pub mod tokens;
 pub use auth::render_auth_tab;
 pub use http::render_http_tab;
 pub use keys::render_keys_tab;
+pub use pkdns::render_pkdns_tab;
 pub use sessions::render_sessions_tab;
 pub use state::{
-    AuthTabState, HttpTabState, KeysTabState, SessionsTabState, StorageTabState, TokensTabState,
+    AuthTabState, HttpTabState, KeysTabState, PkdnsTabState, SessionsTabState, StorageTabState,
+    TokensTabState,
 };
 pub use storage::render_storage_tab;
 pub use tokens::render_tokens_tab;

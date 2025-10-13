3ca29fa779e080ed1329f6a5a10af3b557b81ac8
SHAcollision <127778313+SHAcollision@users.noreply.github.com>
2025-10-13 22:33:45 +0900

    Revert "Fix Android data directory handling (#54)" (#55)
    This reverts commit ecb7b2bd032f73f768c7d5d8fc2b29de5b3c54dd.

--- portable-homeserver/src/app/mod.rs
+++ portable-homeserver/src/app/mod.rs
@@ -1,6 +1,4 @@
 mod admin;
-#[cfg(target_os = "android")]
-mod android;
 mod bootstrap;
 mod config;
 mod state;
@@ -14,10 +12,5 @@ pub use bootstrap::launch_desktop;
 
 #[cfg(target_os = "android")]
 pub use bootstrap::launch_mobile;
-
-#[cfg(target_os = "android")]
-pub(crate) use android::{
-    android_default_data_dir as android_data_dir, ensure_android_environment,
-};
 
 pub use ui::App;
